aee004e301f969aca20ba0538c06ea8b097681cb
Mattin.a <mattin.a@outlook.com>
2024-06-20 20:38:42 +0300

    add Detalis screen

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Image, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { Button, Image, Pressable, StyleSheet } from "react-native";
 import { Stack } from "expo-router";
 import { View, Text } from "@/src/components/Themed";
 import { useLocalSearchParams } from "expo-router";
@@ -7,18 +7,50 @@ import products from "@/assets/data/products";
 import Colors from "@/src/constants/Colors";
 
 export default function ProductDetailsScreen() {
+  const [selectedSize, setSelectedSize] = useState();
   const { id } = useLocalSearchParams();
   const product = products.find((product) => product.id.toString() === id);
+  const sizes = ["S", "M", "L", "XL"];
 
   if (!product) {
     <Text>Product not found!</Text>;
   }
-
+  const AddToCart = () => {
+    alert("Added to Cart");
+  };
   return (
     <View style={styles.container}>
       <Stack.Screen options={{ title: "Details" }} />
       <Image style={styles.image} source={{ uri: product?.image }} />
       <Text style={styles.price}>${product?.price}</Text>
+      <Text>Select Size</Text>
+      <View style={styles.sizes}>
+        {sizes.map((size) => (
+          <Pressable
+            onPress={() => setSelectedSize(size)}
+            style={[
+              styles.sizeContainer,
+              {
+                backgroundColor:
+                  selectedSize === size ? Colors.light.tint : "#fff",
+              },
+            ]}
+            key={size}
+          >
+            <Text
+              style={[
+                styles.textSize,
+                { color: selectedSize === size ? "gray" : "black" },
+              ]}
+            >
+              {size}
+            </Text>
+          </Pressable>
+        ))}
+      </View>
+      <View style={styles.CartButton}>
+        <Button title="Add To Cart" color="#fff" onPress={() => AddToCart()} />
+      </View>
     </View>
   );
 }
@@ -28,9 +60,36 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 20,
   },
+  CartButton: {
+    backgroundColor: Colors.light.tint, // Colors.light.tint export it from Colors.ts
+    width: "70%",
+    borderRadius: 25,
+    alignSelf: "center",
+
+    marginVertical: 20,
+  },
   title: {
     fontSize: 20,
     fontWeight: "bold",
+  },
+  textSize: {
+    fontWeight: "bold",
+    fontSize: 20,
+  },
+  sizes: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginVertical: 20,
+  },
+  sizeContainer: {
+    alignItems: "center",
+    aspectRatio: 1,
+    backgroundColor: "gainsboro",
+    borderRadius: 25,
+    borderWidth: 1,
+    borderColor: Colors.light.tint,
+    justifyContent: "center",
+    width: 50,
   },
   separator: {
     marginVertical: 30,
